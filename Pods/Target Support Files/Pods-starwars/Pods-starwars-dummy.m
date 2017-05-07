6e45ade118bcfc65bf177dcb65da810ee4727fc2
tingxuanz <zhangtx93@gmail.com>
2017-05-07 18:39:46 +1000

    add pod

---
+++ Pods/Target Support Files/Pods-starwars/Pods-starwars-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_starwars : NSObject
+@end
+@implementation PodsDummy_Pods_starwars
+@end
--- starwars/ViewController.swift
+++ starwars/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+  
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        nameTextField.delegate = self
+    }
+    
+    //MARK: UITextFielddelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default"
     }
-
 
 }
 
